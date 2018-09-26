5b380d27ef830136684068a275d60eda69f89470
T Pratik <31564289+pk00095@users.noreply.github.com>
2018-09-27 01:54:35 +0530

    Add files via upload

---
+++ viz_graph.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+from tensorflow.python.platform import gfile
+from tensorflow.core.framework import graph_pb2 as gpb
+from google.protobuf import text_format
+
+with tf.Session() as sess:
+    model_filename ='inception-2015-12-05/classify_image_graph_def.pb'
+    with gfile.FastGFile(model_filename, 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())   # to read from .pb file
+    #graph_def = gpb.GraphDef()
+    #with open(model_filename,'r') as f:
+     #      data=f.read()
+    #text_format.Parse(data, graph_def) # to read from .pbtxt file
+        g_in = tf.import_graph_def(graph_def)
+LOGDIR='./logs/'
+train_writer = tf.summary.FileWriter(LOGDIR)
+train_writer.add_graph(sess.graph)
+train_writer.flush()
+train_writer.close()
+
