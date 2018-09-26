46306c3001bf81c6fa6be7eb69d1efaf4cc8c676
T Pratik <31564289+pk00095@users.noreply.github.com>
2018-09-27 02:09:56 +0530

    Update viz_graph.py

--- viz_graph.py
+++ viz_graph.py
@@ -4,7 +4,7 @@ from tensorflow.core.framework import graph_pb2 as gpb
 from google.protobuf import text_format
 
 with tf.Session() as sess:
-    model_filename ='inception-2015-12-05/classify_image_graph_def.pb'
+    model_filename ='siam.pb'
     with gfile.FastGFile(model_filename, 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())   # to read from .pb file
@@ -14,6 +14,8 @@ with tf.Session() as sess:
     #text_format.Parse(data, graph_def) # to read from .pbtxt file
         g_in = tf.import_graph_def(graph_def)
 LOGDIR='./logs/'
+if not tf.gfile.exists(LOGDIR):
+    tf.gfile.MkDir(LOGDIR)
 train_writer = tf.summary.FileWriter(LOGDIR)
 train_writer.add_graph(sess.graph)
 train_writer.flush()
